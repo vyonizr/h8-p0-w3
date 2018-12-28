53673c841a98e0ca099928a361c40fd30f3f5817
vyonizr <fitrahtur@gmail.com>
2018-12-28 22:43:35 +0700

    Added exercise-5.js

---
+++ exercise-5.js
@@ -0,0 +1,21 @@
+function palindrome(kata) {
+  var terbalik = "";
+
+  for (var i = (kata.length -1); i >= 0; i--) {
+      terbalik += kata[i];
+  }
+
+  if (terbalik === kata) {
+      return true;
+  }
+  else {
+      return false;
+  }
+}
+  
+// TEST CASES
+console.log(palindrome('katak')); // true
+console.log(palindrome('blanket')); // false
+console.log(palindrome('civic')); // true
+console.log(palindrome('kasur rusak')); // true
+console.log(palindrome('mister')); // false
